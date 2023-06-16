8268917aebf1685520c85a9966ead21895e847c9
João Guilherme M. Martins <114001978+xrkmed@users.noreply.github.com>
2023-06-16 19:03:03 -0300

    schema sqlite

--- schema/sqlite.sql
+++ schema/sqlite.sql
@@ -2,8 +2,8 @@
 -- version 5.1.1
 -- https://www.phpmyadmin.net/
 --
--- Host: 127.0.0.1:3306
--- Tempo de geração: 16/05/2023 às 22:43
+-- Host: 127.0.0.1
+-- Tempo de geração: 16/06/2023 às 22:01
 -- Versão do servidor: 10.6.12-MariaDB-cll-lve
 -- Versão do PHP: 7.2.34
 
@@ -33,7 +33,6 @@ CREATE TABLE `tb_amigos` (
   `telefone` bigint(22) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
-
 -- --------------------------------------------------------
 
 --
@@ -42,31 +41,13 @@ CREATE TABLE `tb_amigos` (
 
 CREATE TABLE `tb_emprestimos` (
   `id` int(11) NOT NULL,
-  `startDate` date NOT NULL,
-  `endDate` date NOT NULL,
-  `finalizado` tinyint(1) NOT NULL,
-  `valor_receber` bigint(22) NOT NULL,
-  `amigo_id` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
-
-
--- --------------------------------------------------------
-
---
--- Estrutura para tabela `tb_enderecos`
---
-
-CREATE TABLE `tb_enderecos` (
   `amigo_id` int(11) NOT NULL,
-  `numero` int(8) NOT NULL,
-  `rua` varchar(128) NOT NULL,
-  `bairro` varchar(128) NOT NULL,
-  `cidade` varchar(64) NOT NULL,
-  `uf` varchar(2) NOT NULL,
-  `complemento` varchar(255) NOT NULL,
-  `cep` int(8) NOT NULL
+  `startDate` date NOT NULL,
+  `previsaoDataEntrega` date NOT NULL,
+  `dataFinalizado` date DEFAULT NULL,
+  `valorEmprestimo` bigint(22) NOT NULL DEFAULT 0,
+  `observacoes` text NOT NULL DEFAULT ''
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
-
 
 -- --------------------------------------------------------
 
@@ -77,9 +58,8 @@ CREATE TABLE `tb_enderecos` (
 CREATE TABLE `tb_fabricantes` (
   `id` int(11) NOT NULL,
   `razao_social` varchar(255) NOT NULL,
-  `cnpj` bigint(20) DEFAULT NULL
+  `cnpj` varchar(14) NOT NULL DEFAULT '00000000000000'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
-
 
 -- --------------------------------------------------------
 
@@ -91,8 +71,18 @@ CREATE TABLE `tb_ferramentas` (
   `id` int(22) NOT NULL,
   `name` varchar(22) NOT NULL,
   `price` double NOT NULL,
-  `fabricante_id` int(11) DEFAULT NULL,
-  `emprestimo_id` int(11) DEFAULT NULL
+  `fabricante_id` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura para tabela `tb_ferramentas_emprestimo`
+--
+
+CREATE TABLE `tb_ferramentas_emprestimo` (
+  `emprestimo_id` int(11) NOT NULL,
+  `ferramenta_id` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 --
@@ -103,8 +93,7 @@ CREATE TABLE `tb_ferramentas` (
 -- Índices de tabela `tb_amigos`
 --
 ALTER TABLE `tb_amigos`
-  ADD PRIMARY KEY (`id`),
-  ADD UNIQUE KEY `nome` (`nome`);
+  ADD PRIMARY KEY (`id`);
 
 --
 -- Índices de tabela `tb_emprestimos`
@@ -112,12 +101,6 @@ ALTER TABLE `tb_amigos`
 ALTER TABLE `tb_emprestimos`
   ADD PRIMARY KEY (`id`),
   ADD KEY `amigo_id` (`amigo_id`);
-
---
--- Índices de tabela `tb_enderecos`
---
-ALTER TABLE `tb_enderecos`
-  ADD UNIQUE KEY `amigo_id` (`amigo_id`);
 
 --
 -- Índices de tabela `tb_fabricantes`
@@ -130,8 +113,14 @@ ALTER TABLE `tb_fabricantes`
 --
 ALTER TABLE `tb_ferramentas`
   ADD PRIMARY KEY (`id`),
-  ADD KEY `fabricante_id` (`fabricante_id`),
-  ADD KEY `emprestimo_id` (`emprestimo_id`);
+  ADD KEY `fabricante_id` (`fabricante_id`);
+
+--
+-- Índices de tabela `tb_ferramentas_emprestimo`
+--
+ALTER TABLE `tb_ferramentas_emprestimo`
+  ADD KEY `emprestimo_id` (`emprestimo_id`) USING BTREE,
+  ADD KEY `tb_ferramentas_emprestimo_ibfk_2` (`ferramenta_id`);
 
 --
 -- AUTO_INCREMENT para tabelas despejadas
@@ -141,25 +130,25 @@ ALTER TABLE `tb_ferramentas`
 -- AUTO_INCREMENT de tabela `tb_amigos`
 --
 ALTER TABLE `tb_amigos`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=20;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
 
 --
 -- AUTO_INCREMENT de tabela `tb_emprestimos`
 --
 ALTER TABLE `tb_emprestimos`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
 -- AUTO_INCREMENT de tabela `tb_fabricantes`
 --
 ALTER TABLE `tb_fabricantes`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- AUTO_INCREMENT de tabela `tb_ferramentas`
 --
 ALTER TABLE `tb_ferramentas`
-  MODIFY `id` int(22) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+  MODIFY `id` int(22) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
 
 --
 -- Restrições para tabelas despejadas
@@ -169,20 +158,20 @@ ALTER TABLE `tb_ferramentas`
 -- Restrições para tabelas `tb_emprestimos`
 --
 ALTER TABLE `tb_emprestimos`
-  ADD CONSTRAINT `tb_emprestimos_ibfk_1` FOREIGN KEY (`amigo_id`) REFERENCES `tb_amigos` (`id`);
-
---
--- Restrições para tabelas `tb_enderecos`
---
-ALTER TABLE `tb_enderecos`
-  ADD CONSTRAINT `tb_enderecos_ibfk_1` FOREIGN KEY (`amigo_id`) REFERENCES `tb_amigos` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+  ADD CONSTRAINT `tb_emprestimos_ibfk_1` FOREIGN KEY (`amigo_id`) REFERENCES `tb_amigos` (`id`) ON DELETE CASCADE;
 
 --
 -- Restrições para tabelas `tb_ferramentas`
 --
 ALTER TABLE `tb_ferramentas`
-  ADD CONSTRAINT `tb_ferramentas_ibfk_2` FOREIGN KEY (`emprestimo_id`) REFERENCES `tb_emprestimos` (`id`) ON DELETE SET NULL ON UPDATE CASCADE,
-  ADD CONSTRAINT `tb_ferramentas_ibfk_3` FOREIGN KEY (`fabricante_id`) REFERENCES `tb_fabricantes` (`id`) ON DELETE SET NULL ON UPDATE CASCADE;
+  ADD CONSTRAINT `tb_ferramentas_ibfk_1` FOREIGN KEY (`fabricante_id`) REFERENCES `tb_fabricantes` (`id`) ON UPDATE NO ACTION;
+
+--
+-- Restrições para tabelas `tb_ferramentas_emprestimo`
+--
+ALTER TABLE `tb_ferramentas_emprestimo`
+  ADD CONSTRAINT `tb_ferramentas_emprestimo_ibfk_1` FOREIGN KEY (`emprestimo_id`) REFERENCES `tb_emprestimos` (`id`) ON DELETE CASCADE,
+  ADD CONSTRAINT `tb_ferramentas_emprestimo_ibfk_2` FOREIGN KEY (`ferramenta_id`) REFERENCES `tb_ferramentas` (`id`) ON DELETE CASCADE;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
